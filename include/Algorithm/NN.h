19c88e8d4e71de92e4fcf3c25be864db9cc317fa
Andrea Pietrobon <andrea_pietrobon@hotmail.it>
2024-03-07 14:35:44 +0100

    Update

--- include/Algorithm/NN.h
+++
@@ -1,8 +0,0 @@
-#ifndef NN_H
-#define NN_H
-
-#include "../include/tsp.h"
-
-int nearestNeighbor(int* result, double* cost, instance* inst, int firstNode);
-
-#endif /* NN_H */
--- include/algoSelector.h
+++
@@ -1,8 +0,0 @@
-#ifndef ALGOSELECTOR_H
-#define ALGOSELECTOR_H
-
-#include "../include/tsp.h"
-
-int apply_algorithm(instance* inst);
-
-#endif /* ALGOSELECTOR_H */
