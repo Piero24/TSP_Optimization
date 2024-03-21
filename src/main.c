798bdd9c8791a44c0c1810c0c4618c47e8952bb4
Andrea Pietrobon <andrea_pietrobon@hotmail.it>
2024-03-22 00:32:15 +0100

    minor update

--- src/main.c
+++ src/main.c
@@ -33,6 +33,9 @@ int main(int argc, char** argv)
 
     read_input(&inst);
 
+    // -----------------------------------------------------
+    // Fix all the previous function befor continue with the refactor
+
     apply_algorithm(&inst, AlgorithmName);
 
     show_solution(&inst, true);
